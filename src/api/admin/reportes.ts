6cee7e9b879c2cb32b1f96c78b7c54623d7ae23f
blue <samuelwalter2001@gmail.com>
2024-07-12 00:00:55 -0500

    api para para los reportes de las citas

---
+++ src/api/admin/reportes.ts
@@ -0,0 +1,23 @@
+import { getToken } from "@/api/token-service";
+const URL = import.meta.env.VITE_API_BASE_URL;
+
+export const getConteoCitas = async (request: IConteoCitasRequest): Promise<IConteoCitasResponse> => {
+    const token = getToken();
+    const response = await fetch(`${URL}/reportes/conteoCitas`, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(request)
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        const errorResponse: IMessageResponse = { message: data.message, errors: data.errors };
+        throw { status: response.status, errorResponse };
+    }
+
+    return data as IConteoCitasResponse;
+}
--- src/types/citas.d.ts
+++ src/types/citas.d.ts
@@ -14,3 +14,30 @@ interface IRegistrarPacienteCita{
     motivo: string;
     especialista_id: number;
 }
+
+interface EspecialidadData {
+    nombre: string;
+    total: number;
+}
+
+interface DiasData {
+    dia: string;
+    especialidades: EspecialidadData[];
+}
+
+type IConteoCitasResponse = {
+    fechaInicio: string | null;
+    fechaFin: string | null;
+    citas: {
+        total: number;
+        estudiantes: number;
+        noEstudiantes: number;
+        especialidades: EspecialidadData[] | [];
+    };
+    dias: DiasData[] | [];
+};
+
+interface IConteoCitasRequest {
+    fechaInicio: string | Date | undefined;
+    fechaFin: string | Date | undefined;
+}
